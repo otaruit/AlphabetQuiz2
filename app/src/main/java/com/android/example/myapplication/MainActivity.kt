92dec063f34e3d06b5571e957fcbe3dcd83ac333
natsumekimura <kimuranatsume5656@gmail.com>
2023-06-15 10:52:22 +0900

    クイズデータをテキストファイルに移行 テキストからカンマ区切りで取得する

--- app/src/main/java/com/android/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/android/example/myapplication/MainActivity.kt
@@ -8,9 +8,12 @@ import android.widget.Button
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.android.example.myapplication.databinding.ActivityMainBinding
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
 
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
 
     private lateinit var binding: ActivityMainBinding
@@ -22,85 +25,74 @@ class MainActivity : AppCompatActivity(){
     private val maxQuizCount = 5
 
     //クイズデータ
-    private val quizData = mutableListOf(
-        mutableListOf("a", "A", "R", "P", "D"),
-        mutableListOf("b", "B", "P", "D", "R"),
-        mutableListOf("c", "C", "G", "L", "T"),
-        mutableListOf("d", "D", "Q", "B", "P"),
-        mutableListOf("e", "E", "F", "Y", "K"),
-        mutableListOf("f", "F", "T", "Y", "P"),
-        mutableListOf("g", "G", "C", "L", "Q"),
-        mutableListOf("y", "Y", "H", "V", "T"),
-        mutableListOf("s", "S", "J", "R", "K"),
-        mutableListOf("r", "R", "B", "P", "D"),
-        mutableListOf("k", "K", "F", "X", "Z"),
-        mutableListOf("i", "I", "U", "L", "J"),
-        mutableListOf("l", "L", "V", "I", "T"),
-        mutableListOf("w", "W", "X", "V", "H"),
-        mutableListOf("o", "O", "Q", "D", "P"),
-        mutableListOf("j", "J", "L", "I", "T"),
-        mutableListOf("t", "T", " Y", "V", "U"),
-        mutableListOf("q", "Q", "O", "P", "D"),
-        mutableListOf("h", "H", "K", "F", "L"),
-        mutableListOf("n", "N", "M", "W", "U"),
-        mutableListOf("m", "M", "W", "X", "K"),
-        mutableListOf("p", "P", "B", "R", "D"),
-        mutableListOf("u", "U", "V", "W", "C")
-    )
+    private var quizData = mutableListOf<MutableList<String>>()
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        //intentから取り出す
-//        var getName = this.intent.getStringExtra("LABEL_NAME") //設定したkeyで取り出す
-//        if (getName == "") {
-//            getName = "ななしのごんべ"//Assign default string
-//        }
-
-
-        // 読み出すボタン押下時の挙動。
-//        val buf: BufferedReader = readFile("contents.txt")
-//        val result = buf.use { it.readText() }
-//        binding.labelName.text = "$result　ちゃん"
+        // カウントラベルの更新
+        binding.quizNum.text = "だい" + quizCount.toString() + "もん"
 
         // なまえテキストに表示する
         val prefs = getSharedPreferences("userName", MODE_PRIVATE)
         val name = prefs.getString("userName", "ななしのごんべ")
         binding.labelName.text = "$name ちゃん"
 
-        quizData.shuffle()
         showNextQuiz()
     }
 
-    // 読み出し処理。
-//    private fun readFile(file: String): BufferedReader {
-//        val readFile = File(applicationContext.filesDir, file)
-//        return readFile.bufferedReader()
-//    }
+    // テキストファイルからクイズを取得
+    private fun readFile(file: String) {
+        var `is`: InputStream? = null
+        var br: BufferedReader? = null
+
+        try {
+            try {
+                // assetsフォルダ内の sample.txt をオープンする
+                `is` = this.assets.open(file)
+                br = BufferedReader(InputStreamReader(`is`))
+                var listPortion: MutableList<String>
+
+                // １行ずつ読み込み、改行を付加する
+                var str: String
+                while (br.readLine().also { str = it } != null) {
+                    listPortion = listOf(*str.split(",").toTypedArray()).toMutableList()
+                    quizData.add(listPortion)
+                    println(quizData)
+                }
+
+            } finally {
+                `is`?.close()
+                br?.close()
+            }
+        } catch (e: Exception) {
+            println(e)
+        }
+    }
 
     // クイズを出題する
     private fun showNextQuiz() {
 
-        // カウントラベルの更新
-        binding.quizNum.text = getString(R.string.question_num, quizCount)
+        // quiz_data.txtからクイズデータ読み取り
+        readFile(getString(R.string.textFileName))
+        quizData.shuffle()
 
         // クイズを１問取り出す
-        val quiz = quizData[0]
+        val quiz: MutableList<String> = quizData[0]
 
         // 問題をセット
-        binding.quizText.text = getString(R.string.question_alphabet, quiz[0])
+        binding.quizText.text = quiz[0]
         questionAlphabet = quiz[0]
 
         // 正解をセット
         rightAnswer = quiz[1]
 
-        // 都道府県名を削除
+        // 正解を削除
         quiz.removeAt(0)
 
         // 正解と選択肢３つをシャッフル
@@ -114,7 +106,9 @@ class MainActivity : AppCompatActivity(){
 
         // 出題したクイズを削除する
         quizData.removeAt(0)
+
     }
+
 
     // 解答ボタンが押されたら呼ばれる
     fun checkAnswer(view: View) {
