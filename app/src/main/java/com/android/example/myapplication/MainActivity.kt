e8998775e7d06004fba26d50efcb0aaefb85e42d
natsumekimura <kimuranatsume5656@gmail.com>
2023-07-11 13:56:33 +0900

    修正　動作未確認

--- app/src/main/java/com/android/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/android/example/myapplication/MainActivity.kt
@@ -6,8 +6,11 @@ import android.content.SharedPreferences
 import android.media.MediaPlayer
 import android.os.Build
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -86,10 +89,6 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     private fun showNextQuiz() {
 
-        // 正解/不正解フレームを非表示に
-        val frame: LinearLayout = findViewById(R.id.correct_incorecct_frame)
-        frame.visibility = View.INVISIBLE
-
         // クイズを１問取り出す
         val quiz: MutableList<String> = quizData[0]
 
@@ -138,9 +137,6 @@ class MainActivity : AppCompatActivity() {
     // ラベルの更新
     private fun updateCountLabel(quiz: MutableList<String>) {
 
-        val judgeAnimation: LottieAnimationView = findViewById(R.id.lottie_judge)
-        judgeAnimation.visibility = View.INVISIBLE
-
         binding.quizNum.text = quizCount.toString()
         binding.correctAnswerNum.text = "せいかい　${rightAnswerCount}もん"
         binding.wrongAnswerNum.text = "はずれ　${(quizCount - rightAnswerCount - 1)}もん"
@@ -173,44 +169,67 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+//    // 解答ボタンが押されたら呼ばれる
+//fun checkAnswer(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+//        // フラグメントのレイアウトをインフレートします
+//        val view = inflater.inflate(R.layout.fragment_next_quiz, container, false)
+//
+//        // アニメーションを表示するImageViewを取得します
+//        val animationImageView :LottieAnimationView = view.findViewById<ImageView>(R.id.lottie_judge) as LottieAnimationView
+//
+//        // アニメーションを開始します
+//        animationImageView.setAnimation(R.raw.heart1)
+//        animationImageView.playAnimation()
+//
+//        return view
+//    }
+
+
     // 解答ボタンが押されたら呼ばれる
     @SuppressLint("ClickableViewAccessibility")
     @RequiresApi(Build.VERSION_CODES.O)
     fun checkAnswer(view: View) {
 
-        val judgeAnimation: LottieAnimationView = binding.lottieJudge
-        val answerBtn: Button = findViewById(view.id)
-        val btnText = answerBtn.text.toString()
-
-        if (btnText == rightAnswer) {
-
-            judgeAnimation.setAnimation(R.raw.heart1)
-            judgeAnimation.visibility = View.VISIBLE
-            judgeAnimation.playAnimation()
-
-            //正解の選択肢のボタンの背景色を青に
-            answerBtn.setBackgroundResource(R.drawable.correct_btn_color)
-
-            rightAnswerCount++
-
-        } else {
-            //不正解の時の挙動　不正解アニメーション表示
-            judgeAnimation.setAnimation(R.raw.alien_crying)
-            judgeAnimation.visibility = View.VISIBLE
-            judgeAnimation.playAnimation()
-
-            //選択したの選択肢のボタンの背景色を赤に
-            answerBtn.setBackgroundColor(R.drawable.correct_btn_color)
-
-            //正解の選択肢のボタンの背景色を青に
-        }
 
         btnNotEnabled(false)
 
-        // 正解/不正解フレームを表示
-        val frame: LinearLayout = findViewById(R.id.correct_incorecct_frame)
-        frame.visibility = View.VISIBLE
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, NextQuizFragment())
+            .commit()
+//        val judgeAnimation: LottieAnimationView = binding.lottieJudge
+//        val answerBtn: Button = findViewById(view.id)
+//        val btnText = answerBtn.text.toString()
 
+//        // 正解/不正解フレームを表示
+//        val forwardFrame: LinearLayout = findViewById(R.id.correct_incorecct_frame)
+//        forwardFrame.visibility = View.VISIBLE
+//
+//        // 後ろのフレームを薄く
+////        val behindFrame: LinearLayout = findViewById(R.id.question_frame)
+////        behindFrame.visibility = View.INVISIBLE
+//
+//        if (btnText == rightAnswer) {
+//
+//            judgeAnimation.setAnimation(R.raw.heart1)
+////            judgeAnimation.visibility = View.VISIBLE
+//            judgeAnimation.playAnimation()
+//
+//            //正解の選択肢のボタンの背景色を青に
+//            answerBtn.setBackgroundResource(R.drawable.correct_btn_color)
+//
+//            rightAnswerCount++
+//
+//        } else {
+//            //不正解の時の挙動　不正解アニメーション表示
+//            judgeAnimation.setAnimation(R.raw.alien_crying)
+////            judgeAnimation.visibility = View.VISIBLE
+//            judgeAnimation.playAnimation()
+//
+//            //選択したの選択肢のボタンの背景色を赤に
+//            answerBtn.setBackgroundColor(R.drawable.correct_btn_color)
+//
+//            //正解の選択肢のボタンの背景色を青に
+//        }
 
     }
 
--- app/src/main/java/com/android/example/myapplication/NextQuizFragment.kt
+++ app/src/main/java/com/android/example/myapplication/NextQuizFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.airbnb.lottie.LottieAnimationView
